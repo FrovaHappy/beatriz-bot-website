d6657909d945ba1765e7d8687de6c798808f178e
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-05 17:46:19 -0300

    implement imgur upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,33 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import getBase64 from "@/utils/getBase64";
 export default function Home() {
+  async function submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("submit")
+    const file = new FormData(e.currentTarget).get('file') as File;
+    if (!file) return;
+    let base64Img = await getBase64(file);
+    if (typeof base64Img == 'string') {
+        base64Img = base64Img.replace(/^data:.+base64,/, '')
+    }
+    const result = await fetch('/api/upload', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({image: base64Img}),
+    })
+    const response = await result.json() // response.data is an object containing the image URL
+    console.log(response)
+}
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <form action="" onSubmit={submit}>
+        <input name="file" type="file" maxLength={1}/>
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 }
