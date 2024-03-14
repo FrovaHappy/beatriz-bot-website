b79c580884db9baeb4bd79ba73088c7f06c39e93
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-14 17:59:21 -0300

    implement support to height

--- src/components/useSwitch.tsx
+++ src/components/useSwitch.tsx
@@ -2,12 +2,23 @@
 
 import { useState } from "react";
 import style from "./switch.module.scss";
-
-export default function useSwitch(state = false) {
+import { calculatePercents } from "@/utils/getPadding";
+interface Props {
+  state?: boolean;
+  height?: `${number}px` | `${number}rem`;
+}
+export default function useSwitch({ state = false, height = "2rem" }: Props) {
   const [value, setValue] = useState(state);
 
   const Button = (
-    <div className={style.container}>
+    <div
+      className={style.container}
+      style={{
+        height,
+        width: calculatePercents(height, 1.625),
+        borderRadius: calculatePercents(height, 0.3),
+      }}
+    >
       <input
         type="checkbox"
         onClick={() => setValue(!value)}
@@ -16,7 +27,10 @@ export default function useSwitch(state = false) {
         id="checkbox"
       />
       <label className={style.switch} htmlFor="checkbox">
-        <span className={style.slider}></span>
+        <span
+          className={style.slider}
+          style={{ borderRadius: calculatePercents(height, 0.2) }}
+        ></span>
       </label>
     </div>
   );
