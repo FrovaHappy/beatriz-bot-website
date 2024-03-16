50d255232b590b85a59c11d3c83255e790c4ab5c
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-16 15:14:41 -0300

    add useShapeModifyCtx

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { createContext, useContext, useEffect, useState } from "react";
 import welcome from "./welcome.json";
-import { Canvas } from "@/types/Canvas.types";
+import { Canvas, Layer } from "@/types/Canvas.types";
 import { State } from "@/types/types";
 
 const HomeContext = createContext<State<Canvas> | null>(null);
 const ShapeIdContext = createContext<State<number | null> | null>(null);
+const ShapeModifyContext = createContext<State<Layer | null> | null>(null)
 
 export function useCanvasCtx() {
   const c = useContext(HomeContext);
@@ -19,10 +20,17 @@ export function useShapeIdCtx() {
     throw new Error("this Canvas context is not available in this instance");
   return c;
 }
+export function useShapeModifyCtx() {
+  const c = useContext(ShapeModifyContext);
+  if (!c)
+    throw new Error("this Canvas context is not available in this instance");
+  return c;
+}
 
 export default function Context({ children }: React.PropsWithChildren) {
   const [canvas, setCanvas] = useState(null as unknown as Canvas);
   const [shapeId, setShapeId] = useState<number | null>(null);
+  const [shapeModify, setShapeModify] = useState<Layer | null>(null) 
   useEffect(() => {
     const restoreCanvas =
       JSON.parse(window.localStorage.getItem("canvas") ?? "null") ?? welcome;
@@ -36,7 +44,9 @@ export default function Context({ children }: React.PropsWithChildren) {
   return (
     <HomeContext.Provider value={[canvas, setCanvas]}>
       <ShapeIdContext.Provider value={[shapeId, setShapeId]}>
-        {children}
+        <ShapeModifyContext.Provider value={[shapeModify, setShapeModify]} >
+          {children}
+        </ShapeModifyContext.Provider>
       </ShapeIdContext.Provider>
     </HomeContext.Provider>
   );
