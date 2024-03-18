a6b3930c7e58b712e5a5e1b2a5da465b55904071
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-18 00:44:05 -0300

    add props missing

--- src/app/sections/options/TextOptions.tsx
+++ src/app/sections/options/TextOptions.tsx
@@ -5,7 +5,10 @@ import useSelections from "@/components/useSelections";
 import { Layer, Text } from "@/types/Canvas.types";
 import { useEffect } from "react";
 import style from "./index.module.scss";
+import useColorsInput from "@/components/useColorsInput";
 const LIMIT_CANVAS = 1024;
+const WIDTH_LARGE = '20rem';
+const WIDTH_SHORT = '9.43rem';
 const HEIGHT = `2.625rem`;
 export default function TextOptions({ shape }: { shape: Text }) {
   const [canvas, setCanvas] = useCanvasCtx();
@@ -17,6 +20,7 @@ export default function TextOptions({ shape }: { shape: Text }) {
     max: LIMIT_CANVAS,
     height: HEIGHT,
     title: "X",
+    width: WIDTH_SHORT
   });
   const [y, yInput] = useInputNumber({
     defaultValue: `${shape.y}`,
@@ -25,6 +29,7 @@ export default function TextOptions({ shape }: { shape: Text }) {
     max: LIMIT_CANVAS,
     height: HEIGHT,
     title: "y",
+    width: WIDTH_SHORT
   });
   const [family, familySelector] = useSelections({
     idSelect: shape.family,
@@ -35,6 +40,7 @@ export default function TextOptions({ shape }: { shape: Text }) {
       { id: "Inter", title: "Inter" },
       { id: "Laton", title: "Laton" },
     ],
+    width: WIDTH_LARGE
   });
   const [content, contentInput] = useInputText({
     defaultValue: shape.content,
@@ -46,12 +52,71 @@ export default function TextOptions({ shape }: { shape: Text }) {
     ],
     height: HEIGHT,
     title: "Content",
+    width: WIDTH_LARGE
+  });
+  const [size, sizeInput] = useInputNumber({
+    max: LIMIT_CANVAS,
+    min: 8,
+    step: 1,
+    defaultValue: `${shape.size}`,
+    height: HEIGHT,
+    title: "Size",
+    width: WIDTH_SHORT
+  });
+  const [color, colorInput] = useColorsInput({height: HEIGHT, title: "Color", width: WIDTH_SHORT});
+  const [weight, weightInput] = useInputNumber({
+    max: 1000,
+    min: 1,
+    step: 1,
+    defaultValue: `${shape.weight}`,
+    height: HEIGHT,
+    title: "Weight",
+    width: WIDTH_SHORT
+  });
+  const [limitLetters, limitLettersInput] = useInputNumber({
+    max: 250,
+    min: 1,
+    step: 1,
+    defaultValue: `${shape.limitLetters}`,
+    height: HEIGHT,
+    title: "Limit",
+    width: WIDTH_SHORT
+  });
+  const [align, alignSelector] = useSelections({
+    idSelect: shape.align,
+    height: HEIGHT,
+    title: "Align",
+    values: [
+      { id: "left", title: "left" },
+      { id: "center", title: "center" },
+      { id: "right", title: "right" },
+    ],
+    width: WIDTH_SHORT
+  });
+  const [baseline, baselineSelector] = useSelections({
+    idSelect: shape.baseline,
+    height: HEIGHT,
+    title: "Baseline",
+    values: [
+      { id: "top", title: "top" },
+      { id: "middle", title: "middle" },
+      { id: "bottom", title: "bottom" },
+    ],
+    width: WIDTH_SHORT
   });
   useEffect(() => {
-    shape.x = parseFloat(x ?? "") ?? shape.x;
-    shape.y = parseFloat(y ?? "") ?? shape.y;
+    // Dimensions
+    shape.x = x ?? shape.x;
+    shape.y = y ?? shape.y;
+    // Text
     shape.family = family?.id ?? shape.family;
     shape.content = content ?? shape.content;
+    shape.size = size ?? shape.size;
+    shape.weight = weight ?? shape.weight;
+    shape.color = color ?? shape.color;
+    shape.limitLetters = limitLetters ?? shape.limitLetters;
+    shape.baseline = baseline?.id ?? shape.baseline;
+    shape.align = align?.id ?? shape.align;
     const s = shape as Layer;
     const layers = canvas.layers;
     canvas.layers = layers.map((l) => (l.id === s.id ? s : l));
@@ -67,6 +132,12 @@ export default function TextOptions({ shape }: { shape: Text }) {
       <h3 className={style.title}>Texto</h3>
       {contentInput}
       {familySelector}
+      {sizeInput}
+      {colorInput}
+      {weightInput}
+      {limitLettersInput}
+      {alignSelector}
+      {baselineSelector}
     </>
   );
 }
