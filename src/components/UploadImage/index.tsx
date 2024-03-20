abae8b1e8e9cdf54220f62a2bd58745e08b09593
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-20 18:58:52 -0300

    add EmptyUrl component

--- src/components/UploadImage/index.tsx
+++ src/components/UploadImage/index.tsx
@@ -30,6 +30,19 @@ function WithUrl({ url, onChange }: WithUrlProps) {
     </div>
   );
 }
+
+function EmptyUrl({ onChange }: Omit<WithUrlProps, 'url'>) {
+  return (
+    <>
+      <input id={style.file} type="file" maxLength={1} onChange={onChange} />
+      <label htmlFor={style.file} className={style.emptyUrl} typeof={TYPE_OF}>
+        <IconPencil className={style.emptyUrl__icon}/>
+        Haz click para agregar una imagen.
+      </label>
+    </>
+  );
+}
+
 export default function UploadImage({ defaultValue }: Props): InputExport<Url> {
   const [url, setUrl] = useState(defaultValue);
   const [file, setFile] = useState<File | null>(null);
@@ -38,7 +51,6 @@ export default function UploadImage({ defaultValue }: Props): InputExport<Url> {
   const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     const files = e.target.files;
-    console.log(files);
     if (!files) {
       setUrl(null);
       return;
@@ -60,7 +72,7 @@ export default function UploadImage({ defaultValue }: Props): InputExport<Url> {
           case "string":
             return <WithUrl url={url as string} onChange={onChange} />;
           default:
-            return <div>vació</div>;
+            return <EmptyUrl onChange={onChange}/>;
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
       })()}
