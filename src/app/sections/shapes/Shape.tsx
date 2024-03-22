2c9d179301ec602b22f2eaa288bec4a5de5234a2
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-22 18:56:16 -0300

    implement useDeleteShape

--- src/app/sections/shapes/Shape.tsx
+++ src/app/sections/shapes/Shape.tsx
@@ -9,6 +9,8 @@ import IconTextResize from "@/app/icons/IconTextResize";
 import IconUserSquare from "@/app/icons/IconUserSquare";
 import IconIconsOff from "@/app/icons/IconIconsOff";
 import { useShapeIdCtx } from "@/app/context";
+import IconTrash from "@/app/icons/IconTrash";
+import useDeleteShape from "./useDeleteShape";
 
 interface Props {
   icon: "image" | "text" | "name" | "icon" | string;
@@ -26,7 +28,8 @@ const icons: Record<
   name: IconAt,
 };
 export default function Shape({ image, icon, title, id }: Props) {
-  const [shapeId] = useShapeIdCtx()
+  const [shapeId] = useShapeIdCtx();
+  const onClick = useDeleteShape(id);
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
   const imgDefault =
@@ -42,7 +45,7 @@ export default function Shape({ image, icon, title, id }: Props) {
       ref={setNodeRef}
       {...attributes}
       {...listeners}
-      className={`${style.shape} ${shapeId === id ? style.shape__select : ''}`}
+      className={`${style.shape} ${shapeId === id ? style.shape__select : ""}`}
     >
       <Icon className={style.shape__icon} />
       <h3 className={style.shape__title}>{title}</h3>
@@ -51,6 +54,9 @@ export default function Shape({ image, icon, title, id }: Props) {
         alt="imagen del la capa"
         className={style.shape__image}
       />
+      <button className={style.shape__trash} onMouseUp={onClick}>
+        <IconTrash />
+      </button>
     </li>
   );
 }
