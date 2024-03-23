c89a97d4eafb9af81a0f1a94a4dbd05144034ccf
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-23 17:52:52 -0300

    apply styles

--- src/components/NewShape/index.tsx
+++ src/components/NewShape/index.tsx
@@ -1,8 +1,16 @@
 import { useCanvasCtx, useShapeModifyCtx } from "@/app/context";
 import { Name, Text, Image, Icon } from "@/types/Canvas.types";
 import defaultValue from "./defaultsValues";
+import style from "./index.module.scss";
+import { useState } from "react";
+import IconPlaylistAdd from "@/app/icons/IconPlaylistAdd";
+import IconTextResize from "@/app/icons/IconTextResize";
+import IconAt from "@/app/icons/IconAt";
+import IconPhoto from "@/app/icons/IconPhoto";
+import IconUserSquare from "@/app/icons/IconUserSquare";
 
 export default function NewShape() {
+  const [show, setShow] = useState(false);
   const [canvas, setCanvas] = useCanvasCtx();
   const [, setShapeModify] = useShapeModifyCtx();
 
@@ -14,14 +22,31 @@ export default function NewShape() {
       setCanvas({ ...canvas });
     };
   }
+  const showOptions = show ? style.options : style.options__show;
   return (
     <div>
       <span>{canvas.layers.length} / 10</span>
-      <div>
-        <button onClick={onClick(defaultValue.TEXT)}> + text </button>
-        <button onClick={onClick(defaultValue.NAME)}> + name </button>
-        <button onClick={onClick(defaultValue.IMAGE)}> + image </button>
-        <button onClick={onClick(defaultValue.ICON)}> + icon </button>
+      <div className={style.content}>
+        <button className={style.newButton} onClick={() => setShow(!show)}>
+          <IconPlaylistAdd />
+          new Shape
+        </button>
+        <div className={showOptions}>
+          <button onClick={onClick(defaultValue.TEXT)}>
+            <IconTextResize />
+            text
+          </button>
+          <button onClick={onClick(defaultValue.NAME)}>
+            <IconAt /> name
+          </button>
+          <button onClick={onClick(defaultValue.IMAGE)}>
+            <IconPhoto /> image
+          </button>
+          <button onClick={onClick(defaultValue.ICON)}>
+            <IconUserSquare />
+            icon
+          </button>
+        </div>
       </div>
     </div>
   );
