7451f97149d6e9e2233545e1a08c6b9cb3b9ac58
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-25 23:48:53 -0300

    refactor: remplace swicth cases

--- src/app/sections/options/index.tsx
+++ src/app/sections/options/index.tsx
@@ -1,59 +1,36 @@
-import { useShapeModifyCtx } from "@/app/context";
-import TextOptions from "./TextOptions";
-import { Icon, Image, Name, Text } from "@/types/Canvas.types";
-import { HtmlHTMLAttributes, PropsWithChildren } from "react";
-import style from "./index.module.scss";
-import GeneralOptions from "./GeneralOptions";
-import ImageOptions from "./ImageOptions";
-import NameOptions from "./NameOptions";
-import IconOptions from "./IconOptions";
+import { useShapeModifyCtx } from '@/app/context'
+import TextOptions from './TextOptions'
+import { Icon, Image, Name, Text } from '@/types/Canvas.types'
+import { HtmlHTMLAttributes, PropsWithChildren, useEffect } from 'react'
+import style from './index.module.scss'
+import GeneralOptions from './GeneralOptions'
+import ImageOptions from './ImageOptions'
+import NameOptions from './NameOptions'
+import IconOptions from './IconOptions'
 
-export const LIMIT_CANVAS = 1024;
-export const WIDTH_LARGE = "19.5rem";
-export const WIDTH_SHORT = "9.43rem";
-export const HEIGHT = `2.625rem`;
-function OptionsContent({
-  children,
-  ...props
-}: PropsWithChildren<HtmlHTMLAttributes<any>>) {
+export const LIMIT_CANVAS = 1024
+export const WIDTH_LARGE = '19.5rem'
+export const WIDTH_SHORT = '9.43rem'
+export const HEIGHT = `2.625rem`
+function OptionsContent({ children, ...props }: PropsWithChildren<HtmlHTMLAttributes<any>>) {
   return (
     <div {...props} className={style.content}>
       {children}
     </div>
-  );
+  )
 }
 export default function Options() {
-  const [shape] = useShapeModifyCtx();
-  switch (shape?.type ?? "") {
-    case "text":
-      return (
-        <OptionsContent>
-          <TextOptions shape={shape as Text} />
-        </OptionsContent>
-      );
-    case "image":
-      return (
-        <OptionsContent>
-          <ImageOptions shape={shape as Image} />
-        </OptionsContent>
-      );
-    case "name":
-      return (
-        <OptionsContent>
-          <NameOptions shape={shape as Name} />
-        </OptionsContent>
-      );
-    case "icon":
-      return (
-        <OptionsContent>
-          <IconOptions shape={shape as Icon} />
-        </OptionsContent>
-      );
-    default:
-      return (
-        <OptionsContent>
-          <GeneralOptions />
-        </OptionsContent>
-      );
-  }
+  const [shape] = useShapeModifyCtx()
+
+  const layer = JSON.parse(JSON.stringify(shape))
+  const type = (s: string) => s === shape?.type || s === ''
+  return (
+    <OptionsContent>
+      {!shape ? <GeneralOptions /> : null}
+      {type('text') ? <TextOptions shape={layer as Text} /> : null}
+      {type('image') ? <ImageOptions shape={layer as Image} /> : null}
+      {type('icon') ? <IconOptions shape={layer as Icon} /> : null}
+      {type('name') ? <NameOptions shape={layer as Name} /> : null}
+    </OptionsContent>
+  )
 }
