35c94658d91b5fd5186147987cd4a55975ffa3af
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-25 23:50:42 -0300

    implement NewShape and Canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ import styles from "./page.module.css";
 import Shapes from "./sections/shapes";
 import HomeContext from "./context";
 import Options from "./sections/options";
+import NewShape from '@/components/NewShape'
+import Canvas from '@/components/Canvas'
 export default function Home() {
-
   return (
     <HomeContext>
       <main className={styles.main}>
+        <NewShape />
         <Shapes />
         <Options />
+        <Canvas />
       </main>
     </HomeContext>
-  );
+  )
 }
