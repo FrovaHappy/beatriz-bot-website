196a3ad8f85e08f4d8e82f5f2fe9cbfd7770520e
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-25 23:29:20 -0300

    Update useSetterTimeOut.ts

--- src/components/useSetterTimeOut.ts
+++ src/components/useSetterTimeOut.ts
@@ -5,10 +5,11 @@ import { useEffect, useState } from 'react'
 interface Props<T> {
   time?: number
   value: T
+  defaultValue: T
   setValue(value: T): void
   deps?: any[]
 }
-export default function useSetterTimeOut<T = any>({ value, setValue, time = 100, deps = [] }: Props<T>) {
+export default function useSetterTimeOut<T = any>({ value, setValue, time = 100, deps = [], defaultValue }: Props<T>) {
   const [controller, setController] = useState<T>(value)
   useEffect(() => {
     const resolve = setTimeout(() => {
@@ -18,5 +19,9 @@ export default function useSetterTimeOut<T = any>({ value, setValue, time = 100,
       clearTimeout(resolve)
     }
   }, [controller, ...deps])
+  useEffect(() => {
+    setValue(defaultValue)
+    setController(defaultValue)
+  }, [defaultValue])
   return [controller, setController] as State<T>
 }
