23d50fa3a1fad1232c16f3e0f0608c51ba00c352
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-25 23:50:06 -0300

    formatting file

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -1,43 +1,39 @@
-"use client";
-import { createContext, useContext, useEffect, useState } from "react";
-import welcome from "./welcome.json";
-import { Canvas, Layer } from "@/types/Canvas.types";
-import { State } from "@/types/types";
+'use client'
+import { createContext, useContext, useEffect, useState } from 'react'
+import welcome from './welcome.json'
+import { Canvas, Layer } from '@/types/Canvas.types'
+import { State } from '@/types/types'
 
-const HomeContext = createContext<State<Canvas> | null>(null);
-const ShapeModifyContext = createContext<State<Layer | null> | null>(null);
+const HomeContext = createContext<State<Canvas> | null>(null)
+const ShapeModifyContext = createContext<State<Layer | null> | null>(null)
 
 export function useCanvasCtx() {
-  const c = useContext(HomeContext);
-  if (!c)
-    throw new Error("this Canvas context is not available in this instance");
-  return c;
+  const c = useContext(HomeContext)
+  if (!c) throw new Error('this Canvas context is not available in this instance')
+  return c
 }
 export function useShapeModifyCtx() {
-  const c = useContext(ShapeModifyContext);
-  if (!c)
-    throw new Error("this Canvas context is not available in this instance");
-  return c;
+  const c = useContext(ShapeModifyContext)
+  if (!c) throw new Error('this Canvas context is not available in this instance')
+  return c
 }
 
 export default function Context({ children }: React.PropsWithChildren) {
-  const [canvas, setCanvas] = useState(null as unknown as Canvas);
-  const [shapeModify, setShapeModify] = useState<Layer | null>(null);
+  const [canvas, setCanvas] = useState(null as unknown as Canvas)
+  const [shapeModify, setShapeModify] = useState<Layer | null>(null)
   useEffect(() => {
-    const restoreCanvas =
-      JSON.parse(window.localStorage.getItem("canvas") ?? "null") ?? welcome;
-    setCanvas(restoreCanvas);
-  }, []);
+    const restoreCanvas = JSON.parse(window.localStorage.getItem('canvas') ?? 'null') ?? welcome
+    setCanvas(restoreCanvas)
+  }, [])
   useEffect(() => {
-    if (!canvas) return;
-    window.localStorage.setItem("canvas", JSON.stringify(canvas));
-  }, [canvas]);
-  if (!canvas) return <>building component</>;
+    if (!canvas) return
+    console.log(canvas)
+    window.localStorage.setItem('canvas', JSON.stringify(canvas))
+  }, [canvas])
+  if (!canvas) return <>building component</>
   return (
     <HomeContext.Provider value={[canvas, setCanvas]}>
-      <ShapeModifyContext.Provider value={[shapeModify, setShapeModify]}>
-        {children}
-      </ShapeModifyContext.Provider>
+      <ShapeModifyContext.Provider value={[shapeModify, setShapeModify]}>{children}</ShapeModifyContext.Provider>
     </HomeContext.Provider>
-  );
+  )
 }
