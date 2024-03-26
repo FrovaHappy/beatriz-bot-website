2e293fa56e8002d32df63dbd550a1ad7b925bff2
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-26 16:06:21 -0300

    implement renderImage

--- src/utils/renderCanvas/index.ts
+++ src/utils/renderCanvas/index.ts
@@ -1,27 +1,34 @@
-import { Base, Layer, Text, TextBase, User } from "@/types/Canvas.types";
-import { renderText } from "./renderText";
+import { Base, Layer, Text, TextBase, User, Image } from '@/types/Canvas.types'
+import { renderText } from './renderText'
+
+import { promises } from 'dns'
+import { renderImage } from './renderImage'
 
 interface Path {
-  new (path?: string | Path2D | undefined): Path2D;
-  prototype: Path2D;
+  new (path?: string | Path2D | undefined): Path2D
+  prototype: Path2D
 }
-export default function renderCanvas(
+export default async function renderCanvas(
   layers: Layer[],
   base: Base & TextBase,
   user: User,
   ctx: CanvasRenderingContext2D,
-  Patch2DInstance: Path
+  Patch2DInstance: Path,
+  loadImage: (path: string) => Promise<HTMLImageElement>
 ) {
-  ctx.reset();
+  ctx.reset()
   if (base.color) {
-    ctx.fillStyle = base.color;
-    ctx.fillRect(0, 0, base.width, base.height);
+    ctx.fillStyle = base.color
+    ctx.fillRect(0, 0, base.width, base.height)
   }
   for (const layer of layers) {
     switch (layer.type) {
-      case "text":
-        renderText(layer as Text, ctx, user, base);
-        break;
+      case 'text':
+        renderText(layer as Text, ctx, user, base)
+        break
+      case 'image':
+        await renderImage(layer as Image, ctx, base, loadImage)
+        break
     }
   }
 }
