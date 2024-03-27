de42b3d05b54ec7bea2ecdf6f5b23519726f14be
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-26 22:43:15 -0300

    fix type

--- src/app/sections/options/IconOptions.tsx
+++ src/app/sections/options/IconOptions.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-key */
 import { useCanvasCtx, useShapeModifyCtx } from "@/app/context";
 import style from "./index.module.scss";
-import { Icon, Layer } from "@/types/Canvas.types";
+import { Icon, Layer, Shapes } from '@/types/Canvas.types'
 import { cloneElement, useEffect } from "react";
 import { HEIGHT, LIMIT_CANVAS, WIDTH_LARGE, WIDTH_SHORT } from ".";
 import useInputNumber from "@/components/useInputNumber";
@@ -17,65 +17,65 @@ export default function IconOptions({ shape }: { shape: Icon }) {
     x: useInputNumber({
       defaultValue: `${shape.x}`,
       height: HEIGHT,
-      title: "X",
+      title: 'X',
       placeholder: shape.x.toString(),
       width: WIDTH_SHORT,
       step: 1,
       min: 0,
-      max: LIMIT_CANVAS,
+      max: LIMIT_CANVAS
     }),
     y: useInputNumber({
       defaultValue: `${shape.y}`,
       height: HEIGHT,
-      title: "Y",
+      title: 'Y',
       placeholder: shape.y.toString(),
       width: WIDTH_SHORT,
       step: 1,
       min: 0,
-      max: LIMIT_CANVAS,
+      max: LIMIT_CANVAS
     }),
     width: useInputNumber({
       defaultValue: `${shape.width}`,
       height: HEIGHT,
-      title: "W",
+      title: 'W',
       placeholder: shape.width.toString(),
       width: WIDTH_SHORT,
       step: 1,
       min: 0,
-      max: LIMIT_CANVAS,
+      max: LIMIT_CANVAS
     }),
     height: useInputNumber({
       defaultValue: `${shape.height}`,
       height: HEIGHT,
-      title: "H",
+      title: 'H',
       placeholder: shape.height.toString(),
       width: WIDTH_SHORT,
       step: 1,
       min: 0,
-      max: LIMIT_CANVAS,
+      max: LIMIT_CANVAS
     }),
     title_text: [, <h3 className={style.title}> Texto </h3>],
-    shape: useSelections({
+    shape: useSelections<Shapes>({
       idSelect: shape.shape,
       height: HEIGHT,
-      title: "Forma",
+      title: 'Forma',
       width: WIDTH_LARGE,
       values: [
-        { id: "circle", title: "Circulo" },
-        { id: "square", title: "Cuadrado" },
-        { id: "square5", title: "Borde al 5%" },
-        { id: "square10", title: "Borde al 10%" },
-        { id: "square15", title: "Borde al 15%" },
-        { id: "square20", title: "Borde al 20%" },
-      ],
+        { id: 'circle', title: 'Circulo' },
+        { id: 'square', title: 'Cuadrado' },
+        { id: 'square5', title: 'Borde al 5%' },
+        { id: 'square10', title: 'Borde al 10%' },
+        { id: 'square15', title: 'Borde al 15%' },
+        { id: 'square20', title: 'Borde al 20%' }
+      ]
     }),
     color: useColorsInput({
       defaultValue: shape.color,
       height: HEIGHT,
       width: WIDTH_SHORT,
-      title: "Color",
-    }),
-  };
+      title: 'Color'
+    })
+  }
   const values = Object.keys(options).map(
     (key) => options[key as keyof Omit<Icon, "type">][0]
   );
