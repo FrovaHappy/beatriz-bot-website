d9d2c7dbd3216dd932ed0128ad9505dec110beea
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-28 21:06:08 -0300

    Update Canvas.tsx

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCanvasCtx } from '@/app/context'
 import { User } from '@/types/Canvas.types'
 import renderCanvas from '@/utils/renderCanvas'
