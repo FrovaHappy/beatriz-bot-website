45a17a44fd0a1f6a52c89cb04bebccdbefcc3abc
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-03-28 21:15:25 -0300

    add save and restore ctx

--- src/utils/renderCanvas/renderText.ts
+++ src/utils/renderCanvas/renderText.ts
@@ -1,35 +1,21 @@
-import { Base, Text, TextBase, User } from "@/types/Canvas.types";
-import { userFormatting } from "../formattingText";
+import { Base, Text, TextBase, User } from '@/types/Canvas.types'
+import { userFormatting } from '../formattingText'
 
-export function renderText(
-  text: Text,
-  ctx: CanvasRenderingContext2D,
-  user: User,
-  base: Base & TextBase
-) {
-  const {
-    x,
-    y,
-    size,
-    family,
-    weight,
-    limitLetters,
-    content,
-    align,
-    baseline,
-    color,
-  } = text;
-  let textContent = userFormatting(content, user);
-  ctx.font = `${weight} ${size}px ${family}`;
-  ctx.textAlign = align;
-  ctx.textBaseline = baseline;
+export function renderText(text: Text, ctx: CanvasRenderingContext2D, user: User, base: Base & TextBase) {
+  const { x, y, size, family, weight, limitLetters, content, align, baseline, color } = text
+  let textContent = userFormatting(content, user)
+  ctx.save()
+  ctx.font = `${weight} ${size}px ${family}`
+  ctx.textAlign = align
+  ctx.textBaseline = baseline
   ctx.fillStyle = color ?? base.color ?? 'transparent'
 
-  let lengthLetters = ctx.measureText(textContent);
+  let lengthLetters = ctx.measureText(textContent)
   while (limitLetters < lengthLetters.width && limitLetters !== 0) {
-    textContent = textContent.slice(0, -1);
-    lengthLetters = ctx.measureText(textContent);
+    textContent = textContent.slice(0, -1)
+    lengthLetters = ctx.measureText(textContent)
   }
+  ctx.restore()
 
-  ctx.fillText(textContent, x, y);
+  ctx.fillText(textContent, x, y)
 }
