97a7023c5fa846aa3901dedf42569776f5f8580f
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-04-03 17:56:23 -0300

    implement GenerateJson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+'use client'
 import styles from './page.module.scss'
-import Shapes from "./sections/shapes";
-import HomeContext from "./context";
-import Options from "./sections/options";
+import Shapes from './sections/shapes'
+import HomeContext from './context'
+import Options from './sections/options'
 import NewShape from '@/components/NewShape'
 import Canvas from '@/components/Canvas'
+import GenerateJson from './sections/shapes/GenerateJson'
 export default function Home() {
   return (
     <HomeContext>
@@ -13,6 +14,7 @@ export default function Home() {
         <Shapes />
         <Options />
         <Canvas />
+        <GenerateJson />
       </main>
     </HomeContext>
   )
