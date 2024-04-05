fe1daef1339da338c872e6b6af5dea3ce1537d2b
Frova Happy <61207925+FrovaHappy@users.noreply.github.com>
2024-04-05 19:30:38 -0300

    y param adjusted

--- src/components/NewShape/defaultsValues.ts
+++ src/components/NewShape/defaultsValues.ts
@@ -1,39 +1,26 @@
 import { Icon, Name, Text, Image } from "@/types/Canvas.types";
 
 const TEXT: Text = {
-  type: "text",
+  type: 'text',
   x: 0,
-  y: 0,
-  content: "Example",
+  y: 32,
+  content: 'Example',
   size: 32,
-  family: "Roboto",
+  family: 'Roboto',
   weight: 400,
   limitLetters: 0,
-  align: "start",
-  baseline: "bottom",
-};
+  align: 'start',
+  baseline: 'bottom'
+}
 
 const ICON: Icon = {
-  type: "icon",
+  type: 'icon',
   height: 200,
   width: 200,
   x: 0,
   y: 0,
-  shape: "square",
-};
-
-const NAME: Name = {
-  type: "name",
-  x: 0,
-  y: 0,
-  size: 32,
-  family: "Roboto",
-  weight: 400,
-  limitLetters: 0,
-  align: "start",
-  baseline: "bottom",
-  nameType: "username",
-};
+  shape: 'square'
+}
 
 const IMAGE: Image = {
   type: "image",
@@ -47,7 +34,6 @@ const IMAGE: Image = {
 const defaultValue = {
   TEXT,
   ICON,
-  NAME,
-  IMAGE,
-};
+  IMAGE
+}
 export default defaultValue;
